eb5c61e07aa45d293d9a2bbade9cb361d398caaf
yurison <yu_ri_son@naver.com>
2023-10-06 18:33:06 +0900

    MemberRepository & Test

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -6,23 +6,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-class MemberRepositoryTest {
+class MemberJpaRepositoryTest {
 
     @Autowired
-    MemberRepository memberRepository;
+    MemberJpaRepository memberJpaRepository;
 
     @Test
     void testMember() {
         Member member = new Member("memberA");
-        Member savedMember = memberRepository.save(member);
+        Member savedMember = memberJpaRepository.save(member);
 
-        Member findMember = memberRepository.findById(savedMember.getId()).get();
+        Member findMember = memberJpaRepository.find(savedMember.getId());
 
-        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
     }
