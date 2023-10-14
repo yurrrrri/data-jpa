a6eff9cfd5fa1a68c7ad5c331ac70019acfb8d62
yurison <yu_ri_son@naver.com>
2023-10-14 16:58:13 +0900

    Specification
    JPA Criteria 기반이기 때문에 실무에서 사용하기 어려움

---
+++ src/main/java/study/datajpa/repository/MemberSpec.java
@@ -0,0 +1,24 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
+
+public class MemberSpec {
+
+    public static Specification<Member> teamName(final String teamName) {
+        return (root, query, builder) -> {
+            if (StringUtils.isEmpty(teamName)) return null;
+
+            Join<Member, Team> t = root.join("team", JoinType.INNER);
+            return builder.equal(t.get("name"), teamName);
+        };
+    }
+
+    public static Specification<Member> username(final String username) {
+        return (root, query, builder) -> builder.equal(root.get("username"), username);
+    }
+}
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
@@ -167,5 +168,26 @@ class MemberRepositoryTest {
     @Test
     void callCustom() {
         List<Member> result = memberRepository.findMemberCustom();
+    }
+
+    @Test
+    void specBasic() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        Specification<Member> spec = MemberSpec.username("m1").and(MemberSpec.teamName("teamA"));
+        List<Member> result = memberRepository.findAll(spec);
+
+        // then
+        assertThat(result).hasSize(1);
     }
 }
